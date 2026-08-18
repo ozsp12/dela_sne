90fc1584afea13c6198428b93df0334b1facaff5
Osvaldo L Santos-Pereira <54546141+ozsp12@users.noreply.github.com>
2026-08-18 04:12:48 -0300

    Expose canonical package API

--- src/dela_sne/__init__.py
+++ src/dela_sne/__init__.py
@@ -1,5 +1,22 @@
-"""DELA-SNE research implementation.
+"""Reference software for the DELA-SNE research project."""
 
-The production API will be defined after the mathematical specification in the
-associated manuscript is frozen.
-"""
+from .lac import (
+    LAC,
+    assign_weighted,
+    dispersions_from_labels,
+    distance_matrix,
+    softmax_feature_weights,
+)
+from .tsne import TSNE, joint_probabilities, kl_divergence, kl_gradient
+
+__all__ = [
+    "LAC",
+    "TSNE",
+    "assign_weighted",
+    "dispersions_from_labels",
+    "distance_matrix",
+    "softmax_feature_weights",
+    "joint_probabilities",
+    "kl_divergence",
+    "kl_gradient",
+]
